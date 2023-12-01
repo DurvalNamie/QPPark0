ca1a10672871b775d3ca4fcfe632a51429391e80
Durval Namie <143845151+DurvalNamie@users.noreply.github.com>
2023-12-01 07:36:05 -0300

    teste0

---
+++ dbo.Clientes1.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Clientes] (
+    [Id]             INT            NOT NULL,
+    [Matricula]      INT            NOT NULL,
+    [Nome]           NVARCHAR (100) NOT NULL,
+    [DataNascimento] DATETIME       NOT NULL,
+    [Email]          NVARCHAR (100) NOT NULL,
+    [Telefone]       NVARCHAR (50)  NOT NULL,
+    [Endereco]       NVARCHAR (100) NOT NULL,
+    [Empresa]        NVARCHAR (100) NOT NULL,
+    [Funcao]         NCHAR (10)     NOT NULL,
+    [Ativo]          BIT            NOT NULL,
+    [DataAdmissao]   DATETIME       NOT NULL,
+    CONSTRAINT [PK_dbo.Clientes] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
